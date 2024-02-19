8a8cfe50527bd8ebf2772c11e9cf84d7b6967528
kamal singh <singh540kamal@gmail.com>
2024-02-20 00:22:47 +0530

    Almost complete, Data fetching done

--- script.js
+++ script.js
@@ -2,30 +2,37 @@ const input = document.querySelector('.input');
 const searchButton = document.querySelector('.search');
 
 const weatherDetails = document.querySelector('.weather-details');
-const weatherText = document.querySelectorAll('.weather-text');
-const temprature = document.querySelectorAll('.temprature');
-const dayDate = document.querySelectorAll('.day-date');
-const cityName = document.querySelectorAll('.city-name');
-//SUNNY -> NIGHT -> RAINY
-// console.log(weatherText);
+const weatherText = document.querySelector('.weather-text');
+const temprature = document.querySelector('.temprature');
+const dayDate = document.querySelector('.day-date');
+const cityName = document.querySelector('.city-name');
+
+const rainy = new Set(['Partly cloudy', 'Patchy rain nearby', 'Overcast']);
+const sunny = new Set(['Sunny', 'Clear']);
+const night = new Set(['Mist']);
 
 searchButton.addEventListener('click', () => {
     let place = input.value;
+    input.value = "";
     const fetchData = async function(){
         try {
             let url = `https://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${place}&aqi=no`;
             const res = await fetch(url);
             const data = await res.json();
             const condition = data.current.condition.text;
-            console.log(data.current);
-            if(condition === "Partly cloud" || condition==="Cloud") {
-                weatherText[1].innerText = condition;
-                temprature[1].innerText = data.current.temp_c;
-                dayDate[1].innerText = data.current.last_updated;
-                cityName[1].innerText = place;
-            }
+            weatherText.innerText = condition;
+            temprature.innerText = ` ${data.current.temp_c}`;
+            dayDate.innerText = data.current.last_updated;
+            cityName.innerText = place;
+
+            const classToRemove = weatherDetails.classList.item(1);
+            weatherDetails.classList.remove(classToRemove);
+            if(rainy.has(condition)) weatherDetails.classList.add('weather-rainy');
+            else if(sunny.has(condition)) weatherDetails.classList.add('wather-sunny');
+            else weatherDetails.classList.add('weather-night');
+
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
     fetchData();
