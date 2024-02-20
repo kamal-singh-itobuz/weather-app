327922c2979bb48cde9f011c3d4745252c378d96
kamal singh <singh540kamal@gmail.com>
2024-02-20 22:08:40 +0530

    Date time set

--- script.js
+++ script.js
@@ -13,41 +13,46 @@ const cityName = document.querySelector('.city-name');
 const rainy = new Set(['Partly cloudy', 'Patchy rain nearby', 'Overcast']);
 const sunny = new Set(['Sunny', 'Clear']);
 const night = new Set(['Mist']);
+const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday'];
 
 searchButton.addEventListener('click', () => {
     const place = input.value;
     input.value = "";
-    const fetchData = async function(){
+    const fetchData = async function () {
         try {
             let url = `https://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${place}&aqi=no`;
             const res = await fetch(url);
             const data = await res.json();
-            const condition = data.current.condition.text;
+            const condition = data.current?.condition?.text;
             weatherText.innerText = condition;
             temprature.innerHTML = `${data.current.temp_c}&deg`;
-            dayDate.innerText = data.current.last_updated;
+            const day = (new Date(data.current?.last_updated)).getDay();
+            const month = (new Date(data.current?.last_updated)).getMonth();
+            const date = (new Date(data.current?.last_updated)).getDate();
+            dayDate.innerText = `${dayArray[day]}, ${date} ${monthArray[month]}`;
             cityName.innerText = place;
-            
+
             notFoundCase.style.display = "none";
             const classToRemove = weatherDetails.classList.item(1);
             weatherDetails.classList.remove(classToRemove);
-            if(rainy.has(condition)) weatherDetails.classList.add('weather-rainy');
-            else if(sunny.has(condition)) weatherDetails.classList.add('weather-sunny');
+            if (rainy.has(condition)) weatherDetails.classList.add('weather-rainy');
+            else if (sunny.has(condition)) weatherDetails.classList.add('weather-sunny');
             else weatherDetails.classList.add('weather-night');
 
         } catch (error) {
             notFoundCase.style.display = "flex";
-            weatherText.innerText = 'condition';
-            temprature.innerHTML = `00.0&deg`;
-            dayDate.innerText = 'yyyy-mm-dd 00:00';
-            cityName.innerText = 'location';
+            weatherText.innerText = '----';
+            temprature.innerHTML = `---&deg`;
+            dayDate.innerText = '---- -- --';
+            cityName.innerText = '------';
         }
     }
     fetchData();
 });
 
 window.addEventListener('keypress', (e) => {
-    if(e.key === "Enter"){
+    if (e.key === "Enter") {
         searchButton.click();
     }
 })
