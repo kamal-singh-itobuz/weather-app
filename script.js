a2c0bd6f01191d160d2ad1119b3df11cf4c8d934
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-02-20 11:10:31 +0530

    Complete

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const input = document.querySelector('.input');
 const searchButton = document.querySelector('.search');
 
+const mainContainer = document.querySelector('.main');
+const notFoundCase = document.querySelector('.not-found');
+
 const weatherDetails = document.querySelector('.weather-details');
 const weatherText = document.querySelector('.weather-text');
 const temprature = document.querySelector('.temprature');
@@ -21,20 +24,31 @@ searchButton.addEventListener('click', () => {
             const data = await res.json();
             const condition = data.current.condition.text;
             weatherText.innerText = condition;
-            temprature.innerText = ` ${data.current.temp_c}`;
+            temprature.innerHTML = `${data.current.temp_c}&deg`;
             dayDate.innerText = data.current.last_updated;
             cityName.innerText = place;
-
+            
+            notFoundCase.style.display = "none";
             const classToRemove = weatherDetails.classList.item(1);
             weatherDetails.classList.remove(classToRemove);
             if(rainy.has(condition)) weatherDetails.classList.add('weather-rainy');
-            else if(sunny.has(condition)) weatherDetails.classList.add('wather-sunny');
+            else if(sunny.has(condition)) weatherDetails.classList.add('weather-sunny');
             else weatherDetails.classList.add('weather-night');
 
         } catch (error) {
-            console.log(error);
+            notFoundCase.style.display = "flex";
+            weatherText.innerText = 'condition';
+            temprature.innerHTML = `00.0&deg`;
+            dayDate.innerText = 'yyyy-mm-dd 00:00';
+            cityName.innerText = 'location';
         }
     }
     fetchData();
 });
+
+window.addEventListener('keypress', (e) => {
+    if(e.key === "Enter"){
+        searchButton.click();
+    }
+})
 
